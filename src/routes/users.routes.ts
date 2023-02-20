d509a60d5765748e2692c397339868f022a20066
Anderson Santos <anderood@hotmail.com>
2023-02-20 03:17:43 -0300

    small adjustment

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -13,15 +13,15 @@ usersRoutes.post("/register", (request, response) => {
     const checkUser = accounts.some( user => user.cpf === cpf);
 
     if(checkUser){
-        return response.status(400).send({ "success": false, "msg": "CPF Já Cadastrado!"})
+        return response.status(400).json({ "success": false, "msg": "CPF Já Cadastrado!"})
     }
 
     if(!isCPF(cpf)){
-        return response.status(400).send({ "success": false, "msg": "CPF invalido"})
+        return response.status(400).json({ "success": false, "msg": "CPF invalido"})
     }
 
     if(!isPhone(phone)){
-        return response.status(400).send({ "success": false, "msg": "Telefone invalido"})
+        return response.status(400).json({ "success": false, "msg": "Telefone invalido"})
     }
 
     accounts.push({
@@ -34,7 +34,7 @@ usersRoutes.post("/register", (request, response) => {
         updated_at: new Date()
     });
 
-    return response.status(201).send({ "success": true, "msg": "Cadastro Realizado com Sucesso!"})
+    return response.status(201).json({ "success": true, "msg": "Cadastro Realizado com Sucesso!"})
 });
 
 usersRoutes.get("/search", (request, response ) => {
@@ -44,10 +44,10 @@ usersRoutes.get("/search", (request, response ) => {
     const user = accounts.filter(user => user.cpf === cpf);
 
     if(!!user.length){
-        return response.status(200).send({ "success": true, dataUser: user})
+        return response.status(200).json({ "success": true, dataUser: user})
     }
 
-    return response.status(400).send({ "success": false, "msg": "Informações de CPF não armazenadas"})
+    return response.status(400).json({ "success": false, "msg": "Informações de CPF não armazenadas"})
 });
 
 usersRoutes.put("/user/:id", (request, response) => {
@@ -58,7 +58,7 @@ usersRoutes.put("/user/:id", (request, response) => {
     const userUpdate = accounts.find(item => item.id === id);
     
     if(!userUpdate){
-        return response.status(400).send({success: false, msg: "Usuario não encontrado"})
+        return response.status(400).json({success: false, msg: "Usuario não encontrado"})
     }
 
     userUpdate.first_name = first_name
@@ -66,7 +66,7 @@ usersRoutes.put("/user/:id", (request, response) => {
     userUpdate.phone = phone
      
 
-    return response.status(201).send({ "success": true, dataUser: userUpdate})
+    return response.status(201).json({ "success": true, dataUser: userUpdate})
 });
 
 usersRoutes.delete("/user/:id", (request, response) => {
@@ -77,13 +77,17 @@ usersRoutes.delete("/user/:id", (request, response) => {
     const findIndex = accounts.findIndex(item => item.id === id);
 
     if(findIndex === -1){
-        return response.status(400).send({success: false, msg: "Usuario não encontrado"})
+        return response.status(400).json({success: false, msg: "Usuario não encontrado"})
     }
 
     accounts.splice(findIndex);
 
-    return response.status(204).send();
+    return response.status(204).json();
 
+})
+
+usersRoutes.get("/users", (request, response) => {
+    return response.status(200).json(accounts);
 })
 
 
