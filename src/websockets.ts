ccc3916a834a2fb776f236e46d47da2f9d9762f3
Anderson Santos <anderood@hotmail.com>
2023-03-09 01:52:01 -0300

    add info messages

--- src/websockets.ts
+++ src/websockets.ts
@@ -8,7 +8,15 @@ io.on('connection', (socket) => {
 
   socket.on('chat', (data)=> {
 
-    messages.push(data);
+    const userAccount = accounts.find(item => item.id === data.id)
+
+    messages.push({
+      id: userAccount.id,
+      username: userAccount.first_name,
+      text: data.text,
+      created_at: new Date()
+    });
+    
     socket.broadcast.emit('chatx', messages)
   })
  
